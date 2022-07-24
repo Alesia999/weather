be50df74bdae56691ddcec1ba39d1614958da882
Alesia999 <73301282+Alesia999@users.noreply.github.com>
2022-07-25 00:33:29 +0200

    add tests to city component

--- weather-app/src/app/components/city/city.component.spec.ts
+++ weather-app/src/app/components/city/city.component.spec.ts
@@ -1,11 +1,35 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { WeatherService } from 'src/app/services/weather.service';
-import { WeatherForecast } from 'src/app/interfaces/weather-forecast.interface';
 import { CityComponent } from './city.component';
-import { By } from '@angular/platform-browser';
-import { ReplaySubject, Subject } from 'rxjs';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { of } from 'rxjs';
+import { Weather } from './../../interfaces/weather.interface';
+import { WeatherForecast } from 'src/app/interfaces/weather-forecast.interface';
+import { By } from '@angular/platform-browser';
+
+const weather: Weather = {
+  weatherDescription: 'clouds',
+  weatherImage: '2d',
+  temperature: 25,
+  windSpeed: 3,
+  cityName: 'Torun',
+  countryCode: 'PL',
+};
+
+const forecast: WeatherForecast = [
+  {
+    forecastDate: '2022-07-21 21:00:00',
+    temperature: 25,
+    forecastDescription: 'clouds',
+    forecastImage: '10n',
+  },
+];
+
+let weatherServiceStub: Partial<WeatherService> = {
+  getCurrentWeatherByCity: () => of(weather),
+  getWeatherForecastByCity: () => of(forecast),
+};
 
 describe('CityComponent', () => {
   let component: CityComponent;
@@ -16,13 +40,19 @@ describe('CityComponent', () => {
       imports: [HttpClientTestingModule],
       declarations: [CityComponent],
       schemas: [NO_ERRORS_SCHEMA],
-      providers: [WeatherService],
+      providers: [
+        {
+          provide: WeatherService,
+          useValue: weatherServiceStub,
+        },
+      ],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CityComponent);
     component = fixture.componentInstance;
+
     fixture.detectChanges();
   });
 
@@ -36,17 +66,17 @@ describe('CityComponent', () => {
     expect(spyOnGetWeather).toHaveBeenCalled();
   });
 
-  it('should get weather forecast when onInit', () => {
+  it('should get weather forecast after clicked on city', () => {
     component.ngOnInit();
     expect(component.forecast$).toBeTruthy();
   });
 
   it('should toggle forecast visibility', () => {
+    const forecastComponent = fixture.debugElement.query(By.css('.forecast'));
     component.toggleForecastVisibility();
     expect(component.isForecastVisible).toBeTruthy();
+    fixture.detectChanges();
     component.toggleForecastVisibility();
     expect(component.isForecastVisible).toBeFalsy();
-  })
-
-
+  });
 });
