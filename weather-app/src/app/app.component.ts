05ebc05ff4e1d3a9a33d0b08d36c272ffc8414d2
Alesia999 <73301282+Alesia999@users.noreply.github.com>
2022-07-25 01:52:37 +0200

    add onPush strategy

--- weather-app/src/app/app.component.ts
+++ weather-app/src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  cities = ['Torun', 'Hamburg', 'Stockholm', 'Corfu', 'Lisbon'];
+  cities = of(['Torun', 'Hamburg', 'Stockholm', 'Corfu', 'Lisbon']);
 }
